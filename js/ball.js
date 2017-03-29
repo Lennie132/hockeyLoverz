75547f6b00032b60eb3155113d5f491ac9ed010a
Lennie132 <lennart.v@live.nl>
2017-03-29 17:05:19 +0200

    ball bounces more realistic, goes away when touching the bottom of the game

--- js/ball.js
+++ js/ball.js
@@ -4,71 +4,98 @@
 
 class Ball {
 
-    constructor(x, y) {
-        this._size = 20;
-        this._directionX = 2;
-        this._directionY = 10;
-        this._x = x;
-        this._y = y;
+    constructor(x, y, game) {
+        this._game = game;
+
+        this._radius = 15;
+
+        this._velocityX = 2;
+        this._velocityY = 1;
+        this._coordinateX = x;
+        this._coordinateY = y;
+
+        this._gravity = 0.5;
+        this._traction = 0.8;
+        this._damping = 0.9;
     }
 
 
-    get x() {
-        return this._x;
+    get coordinateX() {
+        return this._coordinateX;
     }
 
-    set x(value) {
-        this._x = value;
+    set coordinateX(value) {
+        this._coordinateX = value;
     }
 
-    get y() {
-        return this._y;
+    get coordinateY() {
+        return this._coordinateY;
     }
 
-    set y(value) {
-        this._y = value;
+    set coordinateY(value) {
+        this._coordinateY = value;
     }
 
-    get size() {
-        return this._size;
+    get velocityX() {
+        return this._velocityX;
     }
 
-    set size(value) {
-        this._size = value;
+    set velocityX(value) {
+        this._velocityX = value;
     }
 
-    get directionX() {
-        return this._directionX;
+    get velocityY() {
+        return this._velocityY;
     }
 
-    set directionX(value) {
-        this._directionX = value;
+    set velocityY(value) {
+        this._velocityY = value;
     }
 
-    get directionY() {
-        return this._directionY;
-    }
-
-    set directionY(value) {
-        this._directionY = value;
-    }
-
-    draw(context, canvas) {
+    draw(canvas, context) {
 
         this.checkBounds(canvas);
 
         context.beginPath();
-        context.fillStyle = "#d62926";
-        context.arc(this._x, this._y, this._size, 0, Math.PI * 2, true);
+        context.fillStyle = "#ffffff";
+        context.border = "black solid 3px";
+        context.arc(this.coordinateX, this.coordinateY, this._radius, 0, Math.PI * 2, false);
         context.closePath();
+
         context.fill();
+
+    }
+    stickHit() {
+        this.velocityY = -this.velocityY * this._damping;
+        this.velocityX *= this._traction;
     }
 
     checkBounds(canvas) {
-        // Boundary Logic
-        if (this._x < 0 || this._x > canvas.width) this._directionX = -this._directionX;
-        if (this._y < 0 || this._y > canvas.height) this._directionY = -this._directionY;
-        this._x += this._directionX;
-        this._y += this._directionY;
+        if (this.coordinateX + this._radius >= canvas.width) {
+            this.velocityX = -this.velocityX * this._damping;
+            this.coordinateX = canvas.width - this._radius;
+
+        } else if (this.coordinateX - this._radius <= 0) {
+            this.velocityX = -this.velocityX * this._damping;
+            this.coordinateX = this._radius;
+        }
+        if (this.coordinateY + this._radius >= canvas.height) {
+            //this.velocityY = -this.velocityY * this.damping;
+            //this.coordinateY = canvas.height - this.radius;
+            //this.velocityX *= this.traction;
+
+            // Destroy ball
+            this._game.destroyBall(this);
+
+
+        } else if (this.coordinateY - this._radius <= 0) {
+            this.velocityY = -this.velocityY * this._damping;
+            this.coordinateY = this._radius;
+        }
+
+        this.velocityY += this._gravity;
+
+        this.coordinateX += this.velocityX;
+        this.coordinateY += this.velocityY;
     }
 }
